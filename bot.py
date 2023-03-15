5d09373db68b9f24174e6cdb3e0cc843a6a22172
Christian Nicdao <chrisnicdao98@gmail.com>
2023-03-14 20:23:39 -0400

    Initial upload

---
+++ bot.py
@@ -0,0 +1,1 @@
+# This file will be mainly used for basic functionality of the bot.
---
+++ main.py
@@ -0,0 +1,7 @@
+# This is the main hub of where the bot will be programmed. The idea behind
+# this bot is that the bot will send the user a message every so often (time intervals tbd),
+# and will ask the user when was the last time they've performed a specific kind of self-care.
+# Future functionality to come!
+
+if __name__ == '__main__':
+    pass
