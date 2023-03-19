8c205cbf411f7d2997308ca3519101ec226f0b6b
Christian Nicdao <chrisnicdao98@gmail.com>
2023-03-19 15:02:40 -0400

    started to implement complements!

--- responses.py
+++ responses.py
@@ -1,14 +1,20 @@
 # This file is going to be used to hold all the files that the bot will use when responding to the user.
+import discord
 
 # Basic test responses
 def test_response(message: str) -> str:
     user_input = message.lower()
 
     if user_input == 'hello':
-        return "Hey you!"
+        return "Hey!"
 
     if user_input == 'why':
-        return ":)"
+        return "cause why not?"
+
+    if user_input == 'send me a complement':
+        return complements
 
     else:
         return "Sorry, didn't catch that!"
+
+complements = "You look great today!"
